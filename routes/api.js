3ea2c40181a2cd0484f929ee79377b1fbfc5d5d3
Foos Mahamud <foosmahamud55@gmail.com>
2020-11-17 23:09:02 -0600

    added workout.js and excercise.html

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,35 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Workout= require("../models/workout.js");
 
-router.post("/api/transaction", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout=> {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
-});
+})
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body}},
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout=> {
+    res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
-    });
+    });   
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
     })
     .catch(err => {
       res.status(400).json(err);
